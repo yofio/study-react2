a108167435a3b4da1238d263632bfd00e7844224
yofio <65844842+yofio@users.noreply.github.com>
2021-06-27 16:23:26 +0900

    onClickOpen

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import { useState } from "react";
+import { ChiledArea } from "./ChiledArea";
 import "./styles.css";
 
 export default function App() {
-  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
+
+  const onChangeText = (e) => setText(e.target.value);
+
+  const onClickOpen = () => setOpen(!open);
 
   const onClickCountUp = () => {
     setCount(count + 1);
   };
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Edit to see some magic happen!</h2>
-      <p>{count}</p>
-      <button onClick={onClickCountUp}>カウントアップ</button>
+      <input value={text} onChange={onChangeText} />
+      <br />
+      <br />
+      <button onClick={onClickOpen}>表示</button>
+      <ChiledArea open={open} />
     </div>
   );
 }
---
+++ src/ChiledArea.jsx
@@ -0,0 +1,17 @@
+const style = {
+  width: "100%",
+  height: "200px",
+  backgroundColor: "khaki"
+};
+export const ChiledArea = (props) => {
+  const { open } = props;
+  return (
+    <>
+      {open ? (
+        <div style={style}>
+          <p>子コンポーネント</p>
+        </div>
+      ) : null}
+    </>
+  );
+};
