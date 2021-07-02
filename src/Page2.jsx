e92275938f4dd19005dec2025af282506f21d91a
yofio <65844842+yofio@users.noreply.github.com>
2021-07-02 14:17:12 +0900

    クエリパラメーターを扱う

--- src/Page2.jsx
+++ src/Page2.jsx
@@ -4,7 +4,9 @@ export const Page2 = () => {
   return (
     <div>
       <h1>Page2です</h1>
-      <Link to="/page2/100">URL parameter</Link>
+      <Link to="/page2/999">URL parameter</Link>
+      <br />
+      <Link to="/page2/999?name=hogehoge">Query parameter</Link>
     </div>
   );
 };
--- src/urlparameter.jsx
+++ src/urlparameter.jsx
@@ -1,11 +1,14 @@
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 
 export const Urlparameter = () => {
   const { id } = useParams();
+  const { search } = useLocation();
+  const query = new URLSearchParams(search);
   return (
     <div>
       <h1>Urlparameterです</h1>
       <p>パラメーターは{id}です</p>
+      <p>クエリパラメーターは{query.get("name")}です</p>
     </div>
   );
 };
