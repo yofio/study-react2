8ed641f3c798478058d6aa476d00beff869a3e53
yofio <65844842+yofio@users.noreply.github.com>
2021-07-03 15:33:57 +0900

    Pageの作成

--- src/App.js
+++ src/App.js
@@ -1,34 +1,6 @@
-import { BrowserRouter } from "react-router-dom";
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SerachInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organisms/user/UserCard";
-import { DefaultLayout } from "./components/templates/DefaultLayout";
-import { HeaderOnly } from "./components/templates/HeaderOnly";
+import { Router2 } from "./router/Rouer2";
 import "./styles.css";
 
-const user = {
-  name: "aaa",
-  email: "dddd@eeeee",
-  phone: "02040402919",
-  image: "https://source.unsplash.com/2l0CWTpcChI",
-  company: {
-    name: "qwqwqw"
-  },
-  website: "https://google.com"
-};
-
 export default function App() {
-  return (
-    <BrowserRouter>
-      <DefaultLayout>
-        <PrimaryButton>テスト</PrimaryButton>
-        <SecondaryButton>検索</SecondaryButton>
-        <br />
-        <SerachInput />
-        <br />
-        <UserCard user={user} />
-      </DefaultLayout>
-    </BrowserRouter>
-  );
+  return <Router2 />;
 }
